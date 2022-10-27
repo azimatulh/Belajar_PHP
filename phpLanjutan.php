3d35f9946d3cdc0ba03511d77b94b1d890b623a4
Azimatul Hanafiyah <afiya611@gmail.com>
2022-10-27 13:14:30 -0700

    Menambah fungsi perulangan dan percabangan

---
+++ phpLanjutan.php
@@ -0,0 +1,128 @@
+<?php
+
+$nama = "Lee Jeno";
+
+/*
+echo $nama;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+echo $nama;
+*/
+
+// Perulangan
+/*
+$no = 100;
+for ($i=0; $i<$no; $i++) {
+    $n = $i + 1;
+    echo $n." ".$nama."<br/>";
+}
+*/
+
+/*
+$no = 10;
+$i = 0;
+while ($i < $no) {
+     $n = $i + 1;
+    echo $n." ".$nama."<br/>";
+    $i++;
+}
+*/
+
+/*
+$no = 10;
+$i = 0;
+do {
+    $n = $i + 1;
+    echo $n." ".$nama."<br/>";
+    $i++;
+} while ($i < $no)
+*/
+ /*
+$data = array('Ferrari', 'Lamborghini', 'Tesla', 'Porsche', 'Hyundai', 'Rubicon');
+$i = 0;
+while ($i < count($data)) {
+    echo $data[$i]."<br>";
+    $i++;
+}
+*/
+// echo $data[5];
+
+/*
+foreach($data as $value) {
+    echo $value."<br>";
+}
+*/
+
+// PERCABANGAN
+/*
+if ($nama == "Lee Jeno") {
+    echo $nama." adalah bias saya";
+} else if($nama == "Yoshi") {
+    echo $nama." bukan bias saya";
+} else {
+    echo $nama." darimana ya?";
+}
+*/
+/*
+switch($nama) {
+    case "Lee jeno":
+        $pesan = $nama." adalah bias say";
+    break;
+    case "Yoshi":
+        $pesan = $nama." bukan bias saya";
+    break;
+    default:
+        $pesan = $nama." darimana ya?";
+}
+echo $pesan;
+*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Input Nama dan Diulang</h1>
+    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+        <label>Nama</label>
+        <input type="text" name="nama">
+        <label>Jumlah</label>
+        <input type="text" name="no">
+        <input type="submit" name="submit" value="Submit">
+    </form>
+    <?php
+        if(!empty($_POST['submit'])) {
+
+            switch($_POST['nama']) {
+                case "Lee Jeno":
+                    $pesan = $_POST['nama']." adalah bias saya";
+                break;
+                case "Yoshi":
+                    $pesan = $_POST['nama']." bukan bias saya";
+                break;
+                default:
+                    $pesan = $_POST['nama']." Bias siapa ya?";
+            
+            }
+
+            for ($i=0;$i<$_POST['no'];$i++) {
+                echo $pesan."<br>";
+            }
+
+        } else {
+            echo "Anda belum input nama dan jumlah";
+        }
+    ?>
+</body>
+</html>
